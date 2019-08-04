234607ed89d05a8973103f22ff3ee21eedc2cb2e
YuriSilenok <yuri.silenok@gmail.com>
2019-08-04 19:25:36 +0300

    init unit test project

---
+++ Lib/DemoUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+	public class DemoUser
+	{
+		/// <summary>
+		/// Возвращает описание тасков
+		/// </summary>
+		/// <returns></returns>
+		public static string[] Tasks()
+		{
+			return new string[] {
+				"Task_1 - сложить два числа переданных во входных параметрах и вернуть как результат функции",
+				"Task_2 - умножить два числа переданных во входных параметрах и вернуть как результат функции" };
+		}
+		/// <summary>
+		/// сложить два числа переданных во входных параметрах и вернуть как результат функции
+		/// </summary>
+		/// <param name="a">Первое слогаемое</param>
+		/// <param name="b">Второе слогаемое</param>
+		/// <returns></returns>
+		public static int Task_1(int a, int b)
+		{
+			return a + b;
+		}
+		/// <summary>
+		/// умножить два числа переданных во входных параметрах и вернуть как результат функции
+		/// </summary>
+		/// <param name="a">первый множитель</param>
+		/// <param name="b">второй множитель</param>
+		/// <returns></returns>
+		public static int Task_2(int a, int b)
+		{
+			return a * b;
+		}
+	}
+}
---
+++ UnitTest/UT_DemoUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class UT_DemoUser
+	{
+		[TestMethod]
+		public void UTest_1()
+		{
+			int a = 3, b = 4, exp = 7;
+			int act = Lib.DemoUser.Task_1(a, b);
+			Assert.AreEqual(exp, act);
+		}
+		[TestMethod]
+		public void UTest_2()
+		{
+			int a = 3, b = 4, exp = 12;
+			int act = Lib.DemoUser.Task_2(a, b);
+			Assert.AreEqual(exp, act);
+		}
+	}
+}
